5f43c49e9106b17b58876869ea69955f0a073532
Ronen Fromchuk <90146636+ronenfromchuk@users.noreply.github.com>
2022-02-07 20:29:05 +0200

    Update test_airline_facade.py

--- tests/test_airline_facade.py
+++ tests/test_airline_facade.py
@@ -26,13 +26,13 @@ def airline_facade_clean():
     repo.reset_db()
 
 def test_get_flights_by_airline(airline_facade_object):
-    assert airline_facade_object.get_flights_by_airline(2) == repo.get_by_column_value(Flights, Flights.airline_company_id, 2)
+    assert airline_facade_object.get_flights_by_airline(1) == repo.get_by_column_value(Flights, Flights.airline_company_id, 1)
 
 def test_not_get_flights_by_airline(airline_facade_object):
     with pytest.raises(InvalidInput):
         airline_facade_object.get_flights_by_airline('e')
     with pytest.raises(AirlineNotFound):
-        airline_facade_object.get_flights_by_airline(3)
+        airline_facade_object.get_flights_by_airline(2)
 
 def test_add_flight(airline_facade_object):
     flight = Flights(airline_company_id=2, origin_country_id=1, destination_country_id=2, departure_time=datetime(2022, 4, 1, 12, 00, 00), landing_time=datetime(2022, 1, 24, 10, 29, 1), remaining_tickets=44)
@@ -41,33 +41,33 @@ def test_add_flight(airline_facade_object):
 
 def test_not_add_flight(airline_facade_object):
     with pytest.raises(InvalidInput):
-        flight = 'Flights(airline_company_id=2, origin_country_id=1, destination_country_id=2, departure_time=datetime(2023, 1, 4, 10, 10, 10), landing_time=datetime(2022, 1, 24, 10, 29, 1), remaining_tickets=44)'
+        flight = 'Flights(airline_company_id=2, origin_country_id=1, destination_country_id=2, departure_time=datetime(2027, 1, 3, 10, 10, 10), landing_time=datetime(2022, 1, 24, 10, 29, 1), remaining_tickets=44)'
         airline_facade_object.add_flight(flight)
     with pytest.raises(InvalidTime):
-        flight = Flights(airline_company_id=2, origin_country_id=1, destination_country_id=2, departure_time=datetime(2023, 1, 4, 10, 10, 10), landing_time=datetime(2022, 1, 24, 10, 29, 1), remaining_tickets=44)
+        flight = Flights(airline_company_id=2, origin_country_id=1, destination_country_id=2, departure_time=datetime(2026, 1, 2, 10, 10, 10), landing_time=datetime(2022, 1, 24, 10, 29, 1), remaining_tickets=44)
         airline_facade_object.add_flight(flight)
     with pytest.raises(NoMoreTicketsLeft):
-        flight = Flights(airline_company_id=2, origin_country_id=1, destination_country_id=2, departure_time=datetime(2022, 4, 1, 12, 00, 00), landing_time=datetime(2022, 1, 24, 10, 29, 1), remaining_tickets=-44)
+        flight = Flights(airline_company_id=2, origin_country_id=1, destination_country_id=2, departure_time=datetime(2025, 2, 2, 12, 00, 00), landing_time=datetime(2022, 1, 24, 10, 29, 1), remaining_tickets=-44)
         airline_facade_object.add_flight(flight)   
     with pytest.raises(InvalidLocation):
-        flight = Flights(airline_company_id=2, origin_country_id=1, destination_country_id=1, departure_time=datetime(2022, 4, 1, 12, 00, 00), landing_time=datetime(2022, 1, 24, 10, 29, 1), remaining_tickets=44)
+        flight = Flights(airline_company_id=2, origin_country_id=1, destination_country_id=1, departure_time=datetime(2024, 3, 2, 12, 00, 00), landing_time=datetime(2022, 1, 24, 10, 29, 1), remaining_tickets=44)
         airline_facade_object.add_flight(flight)
 
 def test_update_airline(airline_facade_object):
-    airline_update = {'name':'Up Yours LTD'}
+    airline_update = {'name':'DEEZ NUTS'}
     airline_facade_object.update_airline(airline_update, 1)
-    assert repo.get_by_column_value(AirlineCompanies, AirlineCompanies.name, 'Up Yours LTD') != None
+    assert repo.get_by_column_value(AirlineCompanies, AirlineCompanies.name, 'EEZ NUTS') != None
 
 def test_not_update_airline(airline_facade_object):
     with pytest.raises(InvalidInput):
-        airline_update = "{'name':'Up Yours LTD'}"
-        airline_facade_object.update_airline(airline_update, 2)
+        airline_update = "{'name':'EEZ NUTS'}"
+        airline_facade_object.update_airline(airline_update, 1)
     with pytest.raises(InvalidInput):
-        airline_update = {'name':'Up Yours LTD'}
-        airline_facade_object.update_airline(airline_update, 'b')
+        airline_update = {'name':'EEZ NUTS'}
+        airline_facade_object.update_airline(airline_update, 'c')
     with pytest.raises(AirlineNotFound):
-        airline_update = {'name':'Up Yours LTD'}
-        airline_facade_object.update_airline(airline_update, 33)
+        airline_update = {'name':'EEZ NUTS'}
+        airline_facade_object.update_airline(airline_update, 21)
 
 def test_update_flight(airline_facade_object):
     flight_update = {'remaining_tickets':12132}
@@ -76,13 +76,13 @@ def test_update_flight(airline_facade_object):
 
 def test_not_update_flight(airline_facade_object):
     with pytest.raises(InvalidInput):
-        airline_facade_object.update_flight("{'remaining_tickets':-2}", 1)
+        airline_facade_object.update_flight("{'remaining_tickets':0}", 1)
     with pytest.raises(InvalidInput):
-        airline_facade_object.update_flight({'remaining_tickets':-2}, 'ty')
+        airline_facade_object.update_flight({'remaining_tickets':-2}, 'np')
     with pytest.raises(NoMoreTicketsLeft):
-        airline_facade_object.update_flight({'remaining_tickets':-2}, 1)
+        airline_facade_object.update_flight({'remaining_tickets':21}, 2)
     with pytest.raises(FlightNotFound):
-        airline_facade_object.update_flight({'remaining_tickets':22}, 52)
+        airline_facade_object.update_flight({'remaining_tickets':2}, 33)
 
 def test_remove_flight(airline_facade_object):
     airline_facade_object.remove_flight(1)
@@ -90,6 +90,6 @@ def test_remove_flight(airline_facade_object):
 
 def test_not_remove_flight(airline_facade_object):
     with pytest.raises(InvalidInput):
-        airline_facade_object.remove_flight('ea')
+        airline_facade_object.remove_flight('na')
     with pytest.raises(FlightNotFound):
         airline_facade_object.remove_flight(77)

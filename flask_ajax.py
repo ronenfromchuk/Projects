6340b2672dc9e7e01a121142422bc881e4c87045
Ronen Fromchuk <90146636+ronenfromchuk@users.noreply.github.com>
2022-02-20 21:49:08 +0200

    Update flask_ajax.py

--- flask_ajax.py
+++ flask_ajax.py
@@ -7,6 +7,34 @@ customers = [{'id': 1, 'name': 'danny', 'address': 'tel-aviv'},
              {'id': 2, 'name': 'marina', 'address': 'beer-sheva'},
              {'id': 3, 'name': 'david', 'address': 'herzeliya'}]
 
+administrators = [{'id': 1, 'name': 'jonny', 'address': 'tel-aviv'},
+             {'id': 2, 'name': 'marcus', 'address': 'beer-sheva'},
+             {'id': 3, 'name': 'andy', 'address': 'herzeliya'}]
+
+users = [{'id': 1, 'name': 'jeses', 'address': 'Manchester'},
+             {'id': 2, 'name': 'Anthony', 'address': 'Salford'},
+             {'id': 3, 'name': 'Patrice', 'address': 'London'}]
+
+countries = [{'id': 1, 'name': 'Israel', 'address': 'Middle-East'},
+             {'id': 2, 'name': 'England', 'address': 'United-Kingdom'},
+             {'id': 3, 'name': 'Netherlands', 'address': 'Europe'}]
+
+flights = [{'id': 1, 'name': 'EL-AL', 'Origin': 'Tel-Aviv', 'destination': 'Kingston'},
+             {'id': 2, 'name': 'British-Airways', 'Origin': 'Manchester', 'destination': 'Tel-Aviv'},
+             {'id': 3, 'name': 'Easy-Jet', 'Origin': 'Manchester', 'destination': 'Ibiza'}]
+
+tickets = [{'id': 1, 'name': 'one', 'destination': 'tel-aviv'},
+             {'id': 2, 'name': 'two', 'destination': 'beer-sheva'},
+             {'id': 3, 'name': 'three', 'destination': 'herzeliya'}]
+
+user_roles = [{'id': 1, 'name': 'jonny', 'user_role': 'administrator'},
+             {'id': 2, 'name': 'British-Airways', 'user_role': 'airline-company'},
+             {'id': 3, 'name': 'andy', 'user_role': 'customer'}]
+
+airlines = [{'id': 1, 'name': 'EL-AL', 'Origin': 'Tel-Aviv'},
+             {'id': 2, 'name': 'British-Airways', 'Origin': 'Manchester'},
+             {'id': 3, 'name': 'Easy-Jet', 'Origin': 'Manchester'}]
+
 # localhost:5000/
 # static page
 # dynamic page
@@ -36,4 +64,82 @@ def get_customer_by_id(id):
             return json.dumps(c)
     return '{}'
 
+@app.route('/conutries', methods = ['GET'])    
+def get_conutries():
+    return json.dumps(countries)
+
+@app.route('/conutries/<int:id>', methods = ['GET'])
+def get_conutry_by_id(id):
+    for c in countries:
+        if c["id"] == id:
+            return json.dumps(c)
+    return '{}'
+
+@app.route('/administrators', methods = ['GET'])    
+def get_administrators():
+    return json.dumps(administrators)
+
+@app.route('/administrators/<int:id>', methods = ['GET'])
+def get_administrator_by_id(id):
+    for c in administrators:
+        if c["id"] == id:
+            return json.dumps(c)
+    return '{}'
+
+@app.route('/users', methods = ['GET'])    
+def get_users():
+    return json.dumps(users)
+
+@app.route('/users/<int:id>', methods = ['GET'])
+def get_user_by_id(id):
+    for c in users:
+        if c["id"] == id:
+            return json.dumps(c)
+    return '{}'
+
+@app.route('/flights', methods = ['GET'])    
+def get_flights():
+    return json.dumps(flights)
+
+@app.route('/flights/<int:id>', methods = ['GET'])
+def get_flight_by_id(id):
+    for c in flights:
+        if c["id"] == id:
+            return json.dumps(c)
+    return '{}'
+
+@app.route('/tickets', methods = ['GET'])    
+def get_tickets():
+    return json.dumps(tickets)
+
+@app.route('/conutries/<int:id>', methods = ['GET'])
+def get_ticket_by_id(id):
+    for c in tickets:
+        if c["id"] == id:
+            return json.dumps(c)
+    return '{}'
+
+@app.route('/user_roles', methods = ['GET'])    
+def get_user_roles():
+    return json.dumps(user_roles)
+
+@app.route('/conutries/<int:id>', methods = ['GET'])
+def get_user_role_by_id(id):
+    for c in user_roles:
+        if c["id"] == id:
+            return json.dumps(c)
+    return '{}'
+
+@app.route('/airlines', methods = ['GET'])    
+def get_airlines():
+    return json.dumps(airlines)
+
+@app.route('/airlines/<int:id>', methods = ['GET'])
+def get_airline_by_id(id):
+    for c in airlines:
+        if c["id"] == id:
+            return json.dumps(c)
+    return '{}'
+
 app.run()
+

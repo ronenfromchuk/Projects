b3109213850bd6cb4ff7c34da7998cc890e87537
Ronen Fromchuk <90146636+ronenfromchuk@users.noreply.github.com>
2022-02-21 19:51:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import tables.Countries
+import tables.Administrators
 from DbRepo import DbRepo
 from db_config import local_session, create_all_entities
 
@@ -9,4 +11,18 @@ repo.reset_db()
 
 repo.create_all_sp('sp_file.sql')
 
+'''
+repo.delete_table('countries')
+repo.delete_table('administrators')
+repo.delete_table('users')
+repo.delete_table('flights')
+repo.delete_table('airline_companies')
+repo.delete_table('tickets')
+repo.delete_table('customers')
+repo.delete_table('user_roles')
+'''
 
+#repo.add(tables.Countries.Countries(name='argentina'))
+#repo.add(tables.Countries.Countries(name='dublin'))
+
+#repo.add(tables.Administrators.Administrators(first_name='bobby', last_name='charlton', user_id=3))
